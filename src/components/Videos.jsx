7abb0c2ebd472c24a01b0f4ecfba014d15a874ce
zhod <abdulmalikabdulhakeem@gmail.com>
2022-09-17 11:00:28 +0100

    second

---
+++ src/components/Videos.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+
+import { ChannelCard, VideoCard } from "./";
+
+const Videos = ({ videos, direction }) => {
+  return (
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos &&
+        videos.map((item, idx) => (
+          <Box key={idx}>
+            {item.id.videoId && <VideoCard video={item} />}
+            {item.id.channelId && <ChannelCard channelDetail={item} />}
+          </Box>
+        ))}
+    </Stack>
+  );
+};
+
+export default Videos;
